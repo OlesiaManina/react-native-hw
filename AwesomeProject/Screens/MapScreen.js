79fd9fbbdd763a72d36ef4c5ac52784dbc99d3e4
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-06-24 21:05:15 +0300

    ДЗ_5

--- AwesomeProject/Screens/MapScreen.js
+++ AwesomeProject/Screens/MapScreen.js
@@ -8,7 +8,7 @@ export const MapScreen = ({route}) => {
 
     return (
         <View style={{flex: 1,}}>
-            <MapView style={{flex: 1,}} showsUserLocation={true} regionegion={{...route.params.location,
+            <MapView style={{flex: 1,}} showsUserLocation={true} region={{...route.params.location,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421}}>
            {location && (<Marker coordinate={{latitude: 37.78825, longitude: -122.4324}} title={route.params.name}/>)}
